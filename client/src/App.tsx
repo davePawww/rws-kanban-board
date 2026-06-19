163b5fd0251ad5089c7e162e8023117a84fadc31
DavePaurillo <paurillo.dave@gmail.com>
2026-06-20 05:01:22 +0800

    chore: setup shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
-  return <h1 className="text-xl font-bold text-red-500 underline">test</h1>;
+  return (
+    <>
+      <h1 className="text-xl font-bold text-red-500 underline">test</h1>
+      <Button size="sm">test</Button>
+    </>
+  );
 }
 
 export default App;

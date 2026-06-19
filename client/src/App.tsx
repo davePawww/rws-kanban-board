c25d5b2eb359908cbe82f0cce021bb1b4a2f06f7
DavePaurillo <paurillo.dave@gmail.com>
2026-06-20 05:07:21 +0800

    chore: added motion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { Button } from '@/components/ui/button';
+import { motion } from 'motion/react';
 
 function App() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: 'easeOut' }}
+    >
       <h1 className="text-xl font-bold text-red-500 underline">test</h1>
       <Button size="sm">test</Button>
-    </>
+    </motion.div>
   );
 }
 
